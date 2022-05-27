28402915052b22be49dca4e545d35f1306a3bbd8
pytomtoto <pytomtoto@gmail.com>
2022-05-27 10:30:54 +0800

    add top vip support
    Signed-off-by: riddle <riddle@hoo.com>

--- go.mod
+++ go.mod
@@ -6,5 +6,3 @@ require (
 	github.com/likexian/gokit v0.25.6
 	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
 )
-
-require github.com/likexian/whois v1.12.5
--- whois_test.go
+++ whois_test.go
@@ -1,27 +1,23 @@
 package whoisparser
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/likexian/whois"
 	"testing"
-	"time"
 )
 
 func TestWhois(t *testing.T) {
-	client := whois.NewClient()
-	client.SetTimeout(5 * time.Second)
-	text, err := client.Whois("baidu.vip")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	info, err := Parse(text)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	b, _ := json.Marshal(info)
-	fmt.Println(string(b))
-	fmt.Println(info.Domain.NameServers)
-	fmt.Println(info.Domain.ExpirationDate)
-	fmt.Println(info.Registrar.Name)
+	//client := whois.NewClient()
+	//client.SetTimeout(5 * time.Second)
+	//text, err := client.Whois("baidu.vip")
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	//info, err := Parse(text)
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+	//b, _ := json.Marshal(info)
+	//fmt.Println(string(b))
+	//fmt.Println(info.Domain.NameServers)
+	//fmt.Println(info.Domain.ExpirationDate)
+	//fmt.Println(info.Registrar.Name)
 }
